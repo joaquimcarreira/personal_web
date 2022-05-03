6a28402af14b6ddf07cc0aceb90c88d004e28019
joaquimcarreira <jcarreira30@gmail.com>
2022-05-03 12:18:35 -0300

    change menu color

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,7 +13,8 @@ export default function Contact() {
             </div>
             <div className="right">
                 <h2>Contact.</h2>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit}
+                >
                     <input type="text" placeholder='Email' />
                     <textarea placeholder='Message'></textarea>
                     <button type='submit'>Submit</button>
